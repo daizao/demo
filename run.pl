72c285a608bc25234e44fa273d666143cf0d5a6e
戴早 <dzbioinformatics@qq.com>
2020-09-18 21:38:27 +0800

    修改

--- run.pl
+++ run.pl
@@ -1,18 +1,5 @@
 #!/usr/bin/perl -w
-use strict;
-use warnings;
-use Cwd;
-
-my $dir=getcwd;
-my @first_file;
-
-opendir(DIR,$dir) || die "no this dir";
-@first_file=readdir(DIR);
-close(DIR);
-my $vina;
-my @doc;
-
-
+#just test file
 for my $i (0..$#first_file){
 	if($first_file[$i] =~ /\.+$/){next};
 	if($first_file[$i] =~ /vina$/){$vina=$dir."/".$first_file[$i]};
@@ -22,21 +9,3 @@ for my $i (0..$#first_file){
 }
 #print `$vina --help`;
 #print @doc;
-
-for my $i (0..$#doc){
-	my $input_1=$dir."/".$doc[$i];
-	my $input_2;
-	opendir(DIR,$input_1) || die "can open $input_1";
-	my @temp=readdir(DIR);	#get the file name in the gene doc
-	close(DIR);
-	for my $j (0..$#temp){
-		if($temp[$j] =~ /\.+$/){next};
-		if(-d $input_1."/".$temp[$j]){
-			$input_2=$input_1."/".$temp[$j];
-		}
-	}
-#	print $input_2;
-	`perl vina_analysis.pl $input_1 $input_2 $vina`;
-}
-
-`perl get_min_energy_mol_list.pl`
